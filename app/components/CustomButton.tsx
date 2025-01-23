1a6421b10f89bf9a78f4e0f66e5346fb35d80e18
AhmedRazaSidd <99049555+AhmedRazaSidd@users.noreply.github.com>
2025-01-23 08:46:21 -0800

    Added: Cars

--- app/components/CustomButton.tsx
+++ app/components/CustomButton.tsx
@@ -18,12 +18,19 @@ const CustomButton = ({
       disabled={false}
       type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
-      onClick={handleClick}
+      onClick={()=> handleClick}
     >
       <span className={`flex-1 ${textStyles}`}>{title}</span>
-      {rightIcon && <div className="relative w-6 h-6">
-        <Image src={rightIcon} alt="Right Icon" fill className="object-contain" />
-        </div>}
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="Right Icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
